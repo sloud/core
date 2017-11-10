25570491598e54cf2804a2c8dd1929e650d747e8
Nicklas Reincke <reincke@pier2port.de>
2017-11-10 07:44:54 +0100

    Changed `ControllerFactory` from implicit to explicit binding

--- src/main/java/com/reynke/sloud/core/controller/IControllerFactory.java
+++ src/main/java/com/reynke/sloud/core/controller/IControllerFactory.java
@@ -1,13 +1,11 @@
 package com.reynke.sloud.core.controller;
 
-import com.google.inject.ImplementedBy;
 import com.reynke.sloud.core.exception.CoreException;
 import com.reynke.sloud.databaseutilities.entity.IEntity;
 
 /**
  * @author Nicklas Reincke (contact@reynke.com)
  */
-@ImplementedBy(ControllerFactory.class)
 public interface IControllerFactory {
 
     IController getController(Class<? extends IEntity> entityType) throws CoreException;
